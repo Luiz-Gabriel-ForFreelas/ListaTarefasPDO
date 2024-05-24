de7be483d705bba2707067411e39fa7af5bf945b
Luiz Gabriel <83136572+Luiz-Gabriel-ForFreelas@users.noreply.github.com>
2024-05-24 13:43:30 -0300

    1.0.1
    Criação de pastas separadas para utilização de classes e funções.

---
+++ app_lista_tarefas/tarefa.model.php
@@ -0,0 +1,20 @@
+<?php
+
+    class Tarefa{
+        private $id;
+        private $id_status;
+        private $tarefa;
+        private $data_cadastro;
+
+        // GETTER
+        public function __get($atributo){
+            return $this->$atributo;
+        }
+
+        // SETTER
+        public function __set($atributo, $valor){
+            $this->$atributo = $valor;
+        }
+    }
+
+?>
